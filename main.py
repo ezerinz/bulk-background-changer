059f71a149fc693e5b13b3c4ae15b57cabc33f20
ezerinz <edwinnnzx@gmail.com>
2023-01-30 00:37:04 +0800

    transparent options

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import os, glob, io
 
 config = ConfigParser()
 config.read("config.cfg")
-input_path = 'input'
-output_path = 'hasil'
-background_color = config.get('config', 'bgcolor')
+input_path = "input"
+output_path = "output"
+background_color = config.get("config", "bgcolor")
+transparent = config.getboolean("config", "transparent")
 
 if not os.path.exists(input_path):
     os.mkdir(input_path)
@@ -15,14 +16,16 @@ if not os.path.exists(input_path):
 if not os.path.exists(output_path):
     os.mkdir(output_path)
 
-for all in glob.glob(input_path+"/*"):
-    filename = os.path.basename(all).rsplit('.', 1)[0]
+for all in glob.glob(input_path + "/*"):
+    filename = os.path.basename(all).rsplit(".", 1)[0]
     input = img.open(all)
     b = io.BytesIO()
-    nobg = remove(input)
-    nobg.save(b, format="png")
-    rembg = img.open(b).convert("RGBA")
-    output = img.new("RGBA", input.size, background_color)
-    output.paste(rembg, mask=rembg)
-    output.convert("RGB").save(output_path+"/"+filename+".png")
+    output = remove(input)
 
+    if not transparent:
+        output.save(b, format="png")
+        rembg = img.open(b).convert("RGBA")
+        output = img.new("RGBA", input.size, background_color)
+        output.paste(rembg, mask=rembg)
+
+    output.save(output_path + "/" + filename + ".png")
